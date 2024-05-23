e85c0b51c670be477af584809c00532cd21dbfcc
zuruzirou <zuruzirou@gmail.com>
2024-05-23 09:24:52 +0900

    a

--- src/main/java/com/example/common/repository/UserRepository.java
+++ src/main/java/com/example/common/repository/UserRepository.java
@@ -1,14 +1,9 @@
 package com.example.common.repository;
 
-import com.example.common.entity.User;
+import com.example.common.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-
-    Optional<User> findByEmail(String email);
 }
